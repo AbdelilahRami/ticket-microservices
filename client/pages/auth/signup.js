fcbd31f841b3faf2b834c4b5b5ebc384c8552bf9
RAMI <ramiabdelilah1@gmail.com>
2020-10-07 18:20:50 +0200

    add client service

---
+++ client/pages/auth/signup.js
@@ -0,0 +1,37 @@
+import {useState} from 'react';
+import useRequest from '../../hooks/use-request';
+import Router from 'next/router';
+
+export default ()=> {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const {doRequest, errors}= useRequest({
+        url :'/api/users/signup',
+        method :'post',
+        body: {
+            email, password
+        },
+        onSuccess: ()=> Router.push('/')
+    });
+
+    const formSubmit=async (event)=>{
+        event.preventDefault();
+        doRequest();
+    }
+
+    return (
+        <form onSubmit= {formSubmit}>
+            <h1>Sign Up</h1>
+            <div className="form-group">
+                <label>Email Adress</label>
+                <input value= {email} onChange={event=>setEmail(event.target.value)} type="text" className="form-control" />
+            </div>
+            <div className="form-group">
+                <label>Password</label>
+                <input value= {password} onChange={ event=>setPassword(event.target.value)} type="password" className="form-control" />
+            </div>
+            {errors}
+            <button className="btn btn-primary">Sign Up</button>
+        </form>
+    )
+}
