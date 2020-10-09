8f7f6794358f34f1ab531875fa8a965d542b82ea
RAMI <ramiabdelilah1@gmail.com>
2020-10-10 01:19:23 +0200

    first dummy test

---
+++ tickets/src/routes/__test__/new.test.ts
@@ -0,0 +1,7 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+it('has a route handler listening to /api/tickets for post requests',  async () => {
+    console.log('test toto');
+
+});
