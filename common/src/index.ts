4f6f80bdbce5d250465a6d49f615b39e32aa34c1
RAMI <ramiabdelilah1@gmail.com>
2020-10-11 00:07:08 +0200

    updates

--- common/src/index.ts
+++ common/src/index.ts
@@ -3,6 +3,8 @@ export * from './errors/custom-error';
 export * from './errors/not-authorized-error';
 export * from './errors/request-validation-errors';
 export * from './errors/error-database-connection';
+export * from './errors/not-found-error';
+
 
 export * from './middlewares/current-user';
 export * from './middlewares/error-handler';
