6e683553545161640feb08adb809bb4ff6025241
RAMI <ramiabdelilah1@gmail.com>
2020-10-22 12:43:05 +0200

    updates

--- common/src/index.ts
+++ common/src/index.ts
@@ -1,17 +1,19 @@
-export * from './errors/bad-request-error';
-export * from './errors/custom-error';
-export * from './errors/not-authorized-error';
-export * from './errors/request-validation-errors';
-export * from './errors/error-database-connection';
-export * from './errors/not-found-error';
+export * from "./errors/bad-request-error";
+export * from "./errors/custom-error";
+export * from "./errors/not-authorized-error";
+export * from "./errors/request-validation-errors";
+export * from "./errors/error-database-connection";
+export * from "./errors/not-found-error";
 
-export * from './middlewares/current-user';
-export * from './middlewares/error-handler';
-export * from './middlewares/require-auth';
-export * from './middlewares/validate-request';
+export * from "./middlewares/current-user";
+export * from "./middlewares/error-handler";
+export * from "./middlewares/require-auth";
+export * from "./middlewares/validate-request";
 
-export * from './events/Subjects';
-export * from './events/listener/base-listener';
-export * from './events/listener/ticket-create-event';
-export * from './events/listener/ticket-update-event';
-export * from './events/publisher/base-publisher';
+export * from "./events/Subjects";
+export * from "./events/listener/base-listener";
+export * from "./events/listener/ticket-create-event";
+export * from "./events/listener/ticket-update-event";
+export * from "./events/publisher/base-publisher";
+
+export * from "./shared/types/order-status";
---
+++ common/src/shared/types/order-status.ts
@@ -0,0 +1,6 @@
+export enum OrderStatus {
+    Created = 'order:created',
+    Cancelled= 'order:cancelled',
+    WaitingPayments= 'order:waiting:payment',
+    Complete= 'order:complete'
+}
