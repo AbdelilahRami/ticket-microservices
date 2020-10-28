223d4f22db996994783d764771b7c097b839f5ba
RAMI <ramiabdelilah1@gmail.com>
2020-10-28 14:04:52 +0100

    crud operations for order without event publisher

--- orders/src/routes/create-order.ts
+++ orders/src/routes/create-order.ts
@@ -1,16 +1,48 @@
-import  mongoose  from 'mongoose';
-import { body } from 'express-validator';
-import express, {Request, Response} from 'express';
-import {requireAuth, requestValidator} from '@arstickets/common';
+import { OrderStatus, currentUser, BadRequestError } from "@arstickets/common";
+import mongoose from "mongoose";
+import { body } from "express-validator";
+import express, { Request, Response } from "express";
+import {
+  requireAuth,
+  requestValidator,
+  NotFoundError,
+} from "@arstickets/common";
+import { Ticket } from "../models/ticket";
+import { Order } from "../models/order";
 const router = express.Router();
 
-router.post('/api/orders',requireAuth,[
-    body('title')
-    .not()
-    .isEmpty()
-    custom((input:string)=> mongoose.Types.ObjectId.isValid(input))
-],async(req:Request, res: Response)=> {
-    res.send({});
-});
+router.post(
+  "/api/orders",
+  requireAuth,
+  [body("ticketId").not().isEmpty().withMessage("ticketId must be provided ")],
+  requestValidator,
+  async (req: Request, res: Response) => {
+    const { ticketId } = req.body;
 
-export { router as createOrderRouter};
+    const ticket = await Ticket.findById(ticketId);
+
+    if (!ticket) {
+      throw new NotFoundError();
+    }
+
+    const isReserved = await ticket.isReserved();
+    if (isReserved) {
+      console.log("ReserVed");
+      throw new BadRequestError("Ticket already reserved");
+    }
+
+    const expiration = new Date();
+    expiration.setMinutes(15);
+
+    const order = Order.build({
+      userId: req.currentUser!.id,
+      status: OrderStatus.Created,
+      expiresAt: expiration,
+      ticket: ticket,
+    });
+    order.save();
+    res.status(201).send(order);
+  }
+);
+
+export { router as createOrderRouter };
--- orders/src/routes/delete-order.ts
+++ orders/src/routes/delete-order.ts
@@ -1,9 +1,24 @@
+import { requireAuth, NotFoundError, OrderStatus, NotAuthorizedError } from '@arstickets/common';
 import express, {Request, Response} from 'express';
+import { Order } from '../models/order';
 
 const router = express.Router();
 
-router.delete('/api/orders/orderId',async(req:Request, res: Response)=> {
-    res.send({});
+router.delete('/api/orders/:orderId',requireAuth,async(req:Request, res: Response)=> {
+    const orderId = req.params.orderId;
+    const order =await Order.findById(orderId);
+
+    if(!order){
+        throw new NotFoundError();
+    }
+
+    if(req.currentUser!.id !== order.userId){
+        throw new NotAuthorizedError();
+    }
+    
+    order.status = OrderStatus.Cancelled;
+    await order.save();
+    res.status(204).send(order);
 });
 
 export { router as deleteOrderRouter};
