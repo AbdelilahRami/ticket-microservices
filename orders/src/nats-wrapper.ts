4e0e8af50e1ef803525f9476c48d4a9e3688db97
RAMI <ramiabdelilah1@gmail.com>
2020-10-29 17:14:42 +0100

    fix natWrapper

--- orders/src/nats-wrapper.ts
+++ orders/src/nats-wrapper.ts
@@ -1,27 +1,27 @@
-import nats,{ Stan } from 'node-nats-streaming';
+import nats, { Stan } from "node-nats-streaming";
 
 class NatsWrapper {
-    private _client!: Stan;
+  private _client!: Stan;
 
-    get client(){
-        if(!this.connect){
-            throw new Error('Cannot acces client before connecting')
-        }
-        return this._client;
+  get client() {
+    if (!this._client) {
+      throw new Error("Cannot acces client before connecting");
     }
-    connect(clusterId: string, clientId: string, url:string){
-        this._client = nats.connect(clusterId, clientId, {url});
+    return this._client;
+  }
+  connect(clusterId: string, clientId: string, url: string) {
+    this._client = nats.connect(clusterId, clientId, { url });
 
-        return new Promise((resolve, reject) => {
-            this.client!.on('connect', () => {
-              console.log('Connected to NATS');
-              resolve();
-            });
-            this.client!.on('error', (err) => {
-              reject(err);
-            });
-          });
-        }
+    return new Promise((resolve, reject) => {
+      this.client!.on("connect", () => {
+        console.log("Connected to NATS");
+        resolve();
+      });
+      this.client!.on("error", (err) => {
+        reject(err);
+      });
+    });
+  }
 }
 
-export const natsWrapper = new NatsWrapper()
+export const natsWrapper = new NatsWrapper();
