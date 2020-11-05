b9d4a80d7297973cedc4a28e5d174cddd73191d3
RAMI <ramiabdelilah1@gmail.com>
2020-11-05 12:21:01 +0100

    test emit event for created order

--- orders/src/routes/__test__/create-order.test.ts
+++ orders/src/routes/__test__/create-order.test.ts
@@ -1,50 +1,70 @@
-import { OrderStatus } from "@arstickets/common";
-import request from "supertest";
-import { app } from "../../app";
-import mongoose from "mongoose";
-import { Ticket } from "../../models/ticket";
-import { Order } from "../../models/order";
+import { OrderStatus } from '@arstickets/common';
+import request from 'supertest';
+import { app } from '../../app';
+import mongoose from 'mongoose';
+import { Ticket } from '../../models/ticket';
+import { Order } from '../../models/order';
+import { natsWrapper } from '../../__mocks__/nats-wrapper';
 
-it("returns en error if ticket not found", async () => {
+it('returns en error if ticket not found', async () => {
   const ticketId = new mongoose.Types.ObjectId().toHexString();
   const response = await request(app)
-    .post("/api/orders/")
-    .set("Cookie", global.signin())
+    .post('/api/orders/')
+    .set('Cookie', global.signin())
     .send({ ticketId })
     .expect(404);
 });
 
-it("returns en error if ticket is already reserved ", async () => {
+it('returns en error if ticket is already reserved ', async () => {
   const ticket = Ticket.build({
-    title: "Match de foot",
+    id: new mongoose.Types.ObjectId().toHexString(),
+    title: 'Match de foot',
     price: 50,
   });
   await ticket.save();
   const order = Order.build({
     ticket,
-    userId: "userId",
+    userId: 'userId',
     status: OrderStatus.Created,
     expiresAt: new Date(),
   });
   await order.save();
 
   const response = await request(app)
-    .post("/api/orders/")
-    .set("Cookie", global.signin())
+    .post('/api/orders/')
+    .set('Cookie', global.signin())
     .send({ ticketId: ticket.id })
     .expect(400);
 });
 
-it("reserves the ticket", async () => {
+it('reserves the ticket', async () => {
   const ticket = Ticket.build({
-    title: "UFC fight",
+    id: new mongoose.Types.ObjectId().toHexString(),
+    title: 'UFC fight',
     price: 500,
   });
   await ticket.save();
 
   const response = await request(app)
-    .post("/api/orders")
-    .set("Cookie", global.signin())
+    .post('/api/orders')
+    .set('Cookie', global.signin())
     .send({ ticketId: ticket.id })
     .expect(201);
+});
+
+it('emits an order created event', async () => {
+  const ticket = Ticket.build({
+    id: mongoose.Types.ObjectId().toHexString(),
+    title: 'concert',
+    price: 20,
+  });
+  await ticket.save();
+
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', global.signin())
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  expect(natsWrapper.client.publish).toBeCalled();
 });
