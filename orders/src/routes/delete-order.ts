40b589b56fa55573248bc84c90ede8e5499a836f
RAMI <ramiabdelilah1@gmail.com>
2020-11-06 11:40:53 +0100

    add orderCancelledPublisher in order-microservice

--- orders/src/routes/delete-order.ts
+++ orders/src/routes/delete-order.ts
@@ -1,3 +1,4 @@
+import { natsWrapper } from './../nats-wrapper';
 import {
   requireAuth,
   NotFoundError,
@@ -5,6 +6,7 @@ import {
   NotAuthorizedError,
 } from "@arstickets/common";
 import express, { Request, Response } from "express";
+import orderCancelledPublisher from "../events/publisher/order-cancelled-publisher";
 import { Order } from "../models/order";
 
 const router = express.Router();
@@ -26,6 +28,17 @@ router.delete(
 
     order.status = OrderStatus.Cancelled;
     await order.save();
+    await new orderCancelledPublisher(natsWrapper.client).publish({
+      id:order.id,
+      userId: order.userId,
+      version:order.version,
+      ticket: {
+        id: order.ticket.id,
+        title: order.ticket.title,
+        price: order.ticket.price
+      },
+      expiresAt: order.expiresAt.toISOString()
+    })
     res.status(204).send(order);
   }
 );
