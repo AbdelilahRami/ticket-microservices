a8c90e27c3944074296e641b31434fe2d6d5f12d
RAMI <ramiabdelilah1@gmail.com>
2020-11-07 10:37:19 +0100

    add expiration-time to order-created-listener

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -1,15 +1,22 @@
 import { expirationQueue } from './../queues/expiration-queue';
 import { Message } from 'node-nats-streaming';
 import { Listener, OrderCreatedEvent, Subjects } from '@arstickets/common';
+
 export class OrderCreatedListenr extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated;
   queueGroupName: string = 'ordre:created';
 
   async onMessage(data: OrderCreatedEvent['data'], message: Message) {
-    console.log('expiration');
-    await expirationQueue.add({
-      orderId: data.id,
-    });
+    const expirationTime =
+      new Date(data.expiresAt).getTime() - new Date().getTime();
+    await expirationQueue.add(
+      {
+        orderId: data.id,
+      },
+      {
+        delay: expirationTime,
+      }
+    );
     message.ack();
   }
 }
